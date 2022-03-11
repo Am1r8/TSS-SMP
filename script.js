735402aaaa566799e87a020176ac37b2d114de00
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-03-10 22:15:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// scrollFun
+jQuery.fn.niceScroll = function() {
+    $(this).click(function(e) {
+        var h = $(this).attr('href'),
+            target;
+
+        if (h.charAt(0) == '#' && h.length > 1 && (target = $(h)).length > 0) {
+            var pos = Math.max(target.offset().top, 0);
+            e.preventDefault();
+            $('body,html').animate({
+                scrollTop: pos
+            }, 'slow', 'swing');
+        }
+    });
+};
+$('.scroll').niceScroll();
+
+function showAlert() {
+    var myText = "Our Server IP is: " + "COMING SOON!";
+    alert(myText);
+}
